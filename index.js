25b80b7284ee9a9a2e69990dfdeaff2d3514a216
Steven Gourley <sgourley@blueskylogic.net>
2023-08-22 14:20:28 +0100

    Upgrade to node18

--- index.js
+++ index.js
@@ -2,36 +2,25 @@
 const core = require('@actions/core');
 const tc = require('@actions/tool-cache');
 
-async function main() {
-    try {
-        const sourceUrl = getDownloadUrl();
-        core.debug(`Downloading from ${sourceUrl}`);
+const IS_WINDOWS = process.platform === 'win32';
+const IS_MAC = process.platform === 'darwin';
 
-        const downloadPath = await tc.downloadTool(sourceUrl);
-        core.debug(`Downloaded to ${downloadPath}`);
-
-        const extractPath = await (IS_WINDOWS ? tc.extractZip(downloadPath) : tc.extractTar(downloadPath));
-        core.debug(`Extracted to ${extractPath}`);
-
-        const cachedPath = await tc.cacheDir(extractPath, 'azcopy', '10');
-        core.addPath(cachedPath);
-        core.debug(`Cached to ${cachedPath}`);
-
-    } catch (err) {
-        core.setFailed(err.toString());
-    }
-}
-
-const IS_WINDOWS = process.platform === 'win32'
-const IS_MAC = process.platform === 'darwin'
-
-function getDownloadUrl() {
-
-    // https://docs.microsoft.com/en-us/azure/storage/common/storage-use-azcopy-v10
-
-    return IS_WINDOWS ? 'https://aka.ms/downloadazcopy-v10-windows'
+try {
+    const sourceUrl = IS_WINDOWS ? 'https://aka.ms/downloadazcopy-v10-windows'
         : IS_MAC ? 'https://aka.ms/downloadazcopy-v10-mac'
             : 'https://aka.ms/downloadazcopy-v10-linux';
-}
 
-main();
+    core.debug(`Downloading from ${sourceUrl}`);
+    const downloadPath = await tc.downloadTool(sourceUrl);
+    core.debug(`Downloaded to ${downloadPath}`);
+
+    const extractPath = await(IS_WINDOWS ? tc.extractZip(downloadPath) : tc.extractTar(downloadPath));
+    core.debug(`Extracted to ${extractPath}`);
+
+    const cachedPath = await tc.cacheDir(extractPath, 'azcopy', '10');
+    core.addPath(cachedPath);
+    core.debug(`Cached to ${cachedPath}`);
+
+} catch (err) {
+    core.setFailed(err.message || err.toString());
+}
